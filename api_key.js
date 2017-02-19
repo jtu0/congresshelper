ade2b76335a9224e4f0bb68b265976df25df6ab5
Jennifer Tu + Chris Lesniewski <jennifertu+lesniewski@gmail.com>
2017-02-18 21:24:06 -0800

    Change static list to fetched list
    List House reps, sorted.

---
+++ api_key.js
@@ -0,0 +1,1 @@
+export var ApiKey='CHANGE_TO_API_KEY';
--- members_list.js
+++ members_list.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import { Text,
 	 ListView,
        } from 'react-native';
+import { ApiKey } from './api_key';
 
 export class MembersList extends Component {
   constructor(props) {
     super(props);
     var dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: dataSource.cloneWithRows(['Barbara Lee', 'Dianne Feinstein', 'Kamala Harris'])
+      dataSource: dataSource.cloneWithRows([])
     }
+    this._getMembersHouseFromApiAsync();
+  }
+
+  async _getMembersHouseFromApiAsync() {
+    try {
+      let response = await this._fetchMembersHouse();
+      let responseJson = await response.json();
+      displayNames = [];
+      for (var result of responseJson.results) {
+        for (var member of result.members) {
+          displayNames.push('(' + member.state + '-' + member.district + ' ' + member.party + ') ' +
+                            member.first_name + ' ' + member.last_name);
+        }
+      }
+      displayNames.sort()
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(displayNames),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  _fetchMembersHouse() {
+    return fetch('https://api.propublica.org/congress/v1/114/house/members.json', {
+      method: 'GET',
+      headers: {
+        'X-API-Key': ApiKey,
+      }
+    });
   }
 
   _renderRow(rowData) {
