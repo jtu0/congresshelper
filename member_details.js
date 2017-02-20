6628882dbaa78bdbfe49a3cb774a8bde06536268
Jennifer Tu + Chris Lesniewski <jennifertu+lesniewski@gmail.com>
2017-02-19 21:57:51 -0800

    Show list of committees in member details page
    ProPublica's data isn't up-to-date, so find the most recent session in which
    the member has committee assignments, and display an end date.

--- member_details.js
+++ member_details.js
@@ -1,7 +1,26 @@
 import React, { Component, PropTypes } from 'react';
-import { View, Text, TouchableHighlight } from 'react-native';
+import { ListView,
+         Text,
+         TouchableHighlight,
+         View,
+       } from 'react-native';
+import { ApiKey } from './api_key';
 
+var member = { committees: [], lastUpdated: null };
 export class MemberDetails extends Component {
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: dataSource.cloneWithRows([]),
+    }
+    var fifty_minutes = 3000000;
+    if (member.lastUpdated === null || Date.now() - member.lastUpdate > fifty_minutes) {
+      this._setMemberCommittees();
+    }
+  }
+
+
   render() {
     m = this.props.member;
     party = m.party=='R' ? 'Republican' :
@@ -13,8 +32,64 @@ export class MemberDetails extends Component {
         <Text>This is the member details page for member {m.id}.</Text>
         <Text>{m.first_name} {m.last_name}</Text>
         <Text>{party} from {state}</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(committee) => this._renderRow(committee)}
+        />
       </View>
     )
+  }
+
+  _renderRow(committee) {
+    text=committee.name + ' (through ' + committee.end_date + ')';
+    return(
+      <Text>{text}</Text>
+    )
+  }
+
+  async _setMemberCommittees() {
+    member.committees = await this._getMemberFromApiAsync();
+    member.lastUpdated = Date.now();
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(member.committees),
+    });
+  }
+
+  async _getMemberFromApiAsync() {
+    try {
+      let response = await this._fetchMember();
+      let responseJson = await response.json();
+      committees = [];
+      for (var result of responseJson.results) {
+        for (var role of result.roles) {
+          // Guard against bad data from Propublica (missing committees in most recent session)
+          if (role.committees.length > 0) {
+            for (var committee of role.committees) {
+              committees.push(committee);
+            }
+            break;
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    return committees;
+  }
+
+  _fetchMember() {
+    m = this.props.member;
+    if (m.api_uri.startsWith('https://api.propublica.org/')) {
+      return fetch(m.api_uri, {
+        method: 'GET',
+        headers: {
+          'X-API-Key': ApiKey,
+        }
+      });
+    }
+    else {
+      console.error('Warning: cross domain request with member uri '+m.api_uri);
+    }
   }
 }
 
